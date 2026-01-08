48cf4f471b613a23de698e067046fc35b1031eed
Arnab Mondal <arnab@thainexus.co.th>
2026-01-08 14:17:29 +0600

    hover 2nd image for products

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -77,11 +77,23 @@ get_header(); ?>
                         $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'woocommerce_thumbnail' );
                         global $product;
                         $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'woocommerce_thumbnail' );
+                        // 2. Get Secondary Image (First image from gallery)
+                        $attachment_ids = $product->get_gallery_image_ids();
+                        $secondary_img_url = '';
+                        if ( $attachment_ids && ! empty($attachment_ids) ) {
+                            // Get the first gallery image
+                            $secondary_img_data = wp_get_attachment_image_src( $attachment_ids[0], 'woocommerce_thumbnail' );
+                            if ( $secondary_img_data ) {
+                                $secondary_img_url = $secondary_img_data[0];
+                            }
+                        }
                         ?>
                         <div class="product-card">
                             <div class="product-img-wrap">
                                 <img src="<?php echo $image ? $image[0] : wc_placeholder_img_src(); ?>" alt="<?php the_title(); ?>">
-                                
+                                <?php if ( $secondary_img_url ) : ?>
+                                    <img src="<?php echo esc_url( $secondary_img_url ); ?>" alt="<?php the_title(); ?>" class="hover-img">
+                                <?php endif; ?>
                                 <button class="wishlist-btn" onclick="toggleWishlist(this)" aria-label="Add to wishlist">
                                     <i class="fa fa-regular fa-heart" aria-hidden="true"></i>
                                 </button>
@@ -153,12 +165,24 @@ get_header(); ?>
                         while ( $all_products_query->have_posts() ) : $all_products_query->the_post();
                             global $product;
                             $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'woocommerce_thumbnail' );
+                            // 2. Get Secondary Image (First image from gallery)
+                            $attachment_ids = $product->get_gallery_image_ids();
+                            $secondary_img_url = '';
+                            if ( $attachment_ids && ! empty($attachment_ids) ) {
+                                // Get the first gallery image
+                                $secondary_img_data = wp_get_attachment_image_src( $attachment_ids[0], 'woocommerce_thumbnail' );
+                                if ( $secondary_img_data ) {
+                                    $secondary_img_url = $secondary_img_data[0];
+                                }
+                            }
                             ?>
                             <div class="swiper-slide">
                                 <div class="product-card">
                                     <div class="product-img-wrap">
                                         <img src="<?php echo $image ? $image[0] : wc_placeholder_img_src(); ?>" alt="<?php the_title(); ?>">
-                                        
+                                        <?php if ( $secondary_img_url ) : ?>
+                                            <img src="<?php echo esc_url( $secondary_img_url ); ?>" alt="<?php the_title(); ?>" class="hover-img">
+                                        <?php endif; ?>
                                         <button class="wishlist-btn" onclick="toggleWishlist(this)" aria-label="Add to wishlist">
                                             <i class="fa fa-regular fa-heart" aria-hidden="true"></i>
                                         </button>
